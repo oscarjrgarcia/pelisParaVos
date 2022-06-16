d78ce9e7e14d4dc47e47b76a14f788d739b9cfd5
“Oscar <oscarogarciajr@gmail.com>
2022-06-16 12:52:19 -0300

    Inicio del proyecto

---
+++ pelisParaVos/src/main/java/app/pelisParaVos/PelisParaVosApplication.java
@@ -0,0 +1,13 @@
+package app.pelisParaVos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelisParaVosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelisParaVosApplication.class, args);
+	}
+
+}
